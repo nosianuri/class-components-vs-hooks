ec240cec4e80d6540864f089afc2d12b771dc90b
nosianuri <nurinosia@gmail.com>
2022-05-12 10:32:30 +0600

    handeling class & hook components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ClassComponent from "./components/ClassComponent";
+
+
+class App extends React.Component {
+  render() {
+    return <ClassComponent />;
+  }
+}
+
+export default App;
